c71d3353ce0752806a9abbc567de3594bc0dc03f
dougbrom <doug@develodesign.co.uk>
2015-06-16 11:06:17 +0100

    Initial import of files

---
+++ code/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Develo_PaymentByAdminIp_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ code/Model/PaymentByAdminIp.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Payment method for allowing payment by certain IPs chosen in the admin.
+ *
+ * @category    Develo
+ * @package     Develo_PaymentByAdminIp
+ * @author      Doug Bromley <doug@develodesign.co.uk>
+ * @copyright   Develo Design 2015
+ */
+
+class Develo_PaymentByAdminIp_Model_PaymentByAdminIp extends Mage_Payment_Model_Method_Abstract
+{
+    protected $_code = 'paymentbyadminip';
+    protected $_canUseInternal = true;
+
+    public function isAvailable($quote = null)
+    {
+        if (!$quote) {
+            return false;
+        }
+
+        if (parent::isAvailable($quote)) {
+            if ($this->_checkCustomerIp()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function _checkCustomerIp()
+    {
+        $customerIp = Mage::helper('core/http')->getRemoteAddr();
+        $allowedIpsConfig = Mage::getStoreConfig('payment/paymentbyadminip/allowed_ips');
+
+
+        if (strpos($allowedIpsConfig, ',') !== false) {
+            $allowedIps = explode(',', $allowedIpsConfig);
+            if (in_array($customerIp, $allowedIps)) {
+                return true;
+            }
+        }
+
+        if ($allowedIpsConfig == $customerIp) {
+            return true;
+        }
+
+        return false;
+    }
+}
